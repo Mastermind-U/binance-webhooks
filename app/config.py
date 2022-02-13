a3620db557a643fdd3a1db5e76a6ac2649a4730e
Mastermind-U <rex49513@gmail.com>
2022-02-13 19:23:32 +0300

    added passphrase

--- app/config.py
+++ app/config.py
@@ -1,31 +1,34 @@
 import os
-
-from pydantic import BaseSettings
 from functools import lru_cache
+
 from hvac import Client
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     DEBUG: bool
     API_KEY: str
     SECRET_KEY: str
+    PASSPHRASE: str
 
 
 @lru_cache
 def get_settings():
+    """Get settings from vault and env."""
     client = Client(
         url='http://vault:8200',
         token=os.environ['VAULT_ROOT_TOKEN'],
     )
     if not client.is_authenticated():
-        raise Exception
+        raise Exception('Vault auth error')
 
     binance_data = client.secrets.kv.v1.read_secret(
         path='BINANCE',
         mount_point='kv',
-    )
+    )['data']
 
     return Settings(
-        API_KEY=binance_data['data']['API_KEY'],
-        SECRET_KEY=binance_data['data']['API_SECRET'],
+        API_KEY=binance_data['API_KEY'],
+        SECRET_KEY=binance_data['API_SECRET'],
+        PASSPHRASE=binance_data['PASSPHRASE'],
     )
