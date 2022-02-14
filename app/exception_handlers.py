12ab574d32a96a025ea6b12c1fe19b7b0c6f5aad
Mastermind-U <rex49513@gmail.com>
2022-02-14 13:57:32 +0300

    added error log

--- app/exception_handlers.py
+++ app/exception_handlers.py
@@ -3,6 +3,7 @@
 from binance import exceptions
 from fastapi import Request
 from fastapi.responses import JSONResponse
+from loguru import logger
 
 BINANCE_EXCEPTIONS = (
     exceptions.BinanceAPIException,
@@ -23,4 +24,5 @@ async def binance_exception_handler(
     exc: exceptions.BinanceAPIException,
 ):
     """Handle any binance exception."""
+    logger.error(f"Catched binance error: {exc}")
     return JSONResponse(status_code=500, content={'detail': str(exc)})
