114e08228af3f1d06947ce07efa865cad497932e
Mastermind-U <rex49513@gmail.com>
2022-02-16 07:19:47 +0300

    removed unnes models

--- app/models.py
+++ app/models.py
@@ -4,31 +4,11 @@ from decimal import Decimal
 from pydantic import BaseModel
 
 
-class Bar(BaseModel):
-    time: datetime
-    open: Decimal
-    high: Decimal
-    low: Decimal
-    close: Decimal
-    volume: Decimal
-
-
-class Strategy(BaseModel):
-    position_size: int
-    order_action: str
-    order_contracts: Decimal
-    order_price: Decimal
-    order_id: str
-    market_position: str
-    market_position_size: int
-    prev_market_position: str
-    prev_market_position_size: int
-
-
 class WebhookData(BaseModel):
     passphrase: str
     time: datetime
+    bar_time: datetime
     exchange: str
     ticker: str
-    bar: Bar
-    strategy: Strategy
+    order_id: str
+    order_action: str
